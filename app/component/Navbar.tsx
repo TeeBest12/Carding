90568d36f72df801e18e4c47ca1e6b60447fa116
TeeBest12 <adegbolatheophilus12@gmail.com>
2026-06-23 11:45:32 +0100

    sksk

--- app/component/Navbar.tsx
+++ app/component/Navbar.tsx
@@ -30,7 +30,7 @@ export default function Navbar() {
       <div className="flex gap-3">
         <a className="hover:opacity-75" href="">Home</a>
         <a  className="hover:opacity-75"href="/sell-gift-card">Sell Gift Card</a>
-        <a className="hover:opacity-75"href="/">FAQ</a>
+        <a className="hover:opacity-75"href="/faq">FAQ</a>
         <a className="hover:opacity-75" href="/download-app">Download App</a>
       </div>
 
